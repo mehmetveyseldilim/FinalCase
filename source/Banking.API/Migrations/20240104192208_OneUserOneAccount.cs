0e3e1a4ebd9956ad903e37d347909cdbf3fa35f0
mehmetveyseldilim <veyseldilim@gmail.com>
2024-01-07 02:41:42 +0300

    bills table & now user can only have one account

---
+++ source/Banking.API/Migrations/20240104192208_OneUserOneAccount.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Banking.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class OneUserOneAccount : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Accounts_UserId",
+                schema: "BankingSchema",
+                table: "Accounts");
+
+            migrationBuilder.CreateTable(
+                name: "Bills",
+                schema: "BankingSchema",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Amount = table.Column<int>(type: "integer", nullable: false),
+                    LastPayTime = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    IsActive = table.Column<bool>(type: "boolean", nullable: false),
+                    AccountId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Bills", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Bills_Accounts_AccountId",
+                        column: x => x.AccountId,
+                        principalSchema: "BankingSchema",
+                        principalTable: "Accounts",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Accounts_UserId",
+                schema: "BankingSchema",
+                table: "Accounts",
+                column: "UserId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Bills_AccountId",
+                schema: "BankingSchema",
+                table: "Bills",
+                column: "AccountId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Bills",
+                schema: "BankingSchema");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Accounts_UserId",
+                schema: "BankingSchema",
+                table: "Accounts");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Accounts_UserId",
+                schema: "BankingSchema",
+                table: "Accounts",
+                column: "UserId");
+        }
+    }
+}
